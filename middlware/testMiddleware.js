0ca5d3af1f83a646c29038c9e16fa54bfb25f478
Raymond Lim <rlim8868@gmail.com>
2022-07-18 18:34:20 +1000

    fixed testmiddleware

--- middlware/testMiddleware.js
+++ middlware/testMiddleware.js
@@ -1,18 +1,18 @@
 const asyncHandler = require('express-async-handler')
 
-const testMiddleware = (req, res, next) => {
+const testMiddleware = asyncHandler(async(req, res, next) => {
     console.log('test 1')
     req.num = 1;
     req.test1 = 'first middleware has run'
     next()
-}
+})
 
-const testMiddleware2 = (req, res, next) => {
+const testMiddleware2 = asyncHandler(async(req, res, next) => {
     console.log('test 2')
     req.num = req.num + 2;
     req.test2 = 'second middleware has run'
     next()
-}
+})
 
 module.exports = {
     testMiddleware,
